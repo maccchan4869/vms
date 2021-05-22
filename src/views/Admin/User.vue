2ec04c7e943bc7c68b3232051a0cea446ec2e06d
maccchan4869 <ymatsio554@gmail.com>
2021-05-22 09:38:32 +0900

    スタッフ検索・ソート機能実装

--- src/views/Admin/User.vue
+++ src/views/Admin/User.vue
@@ -8,6 +8,13 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-right">
+            <label class="mx-2">検索</label>
+            <label class="mx-2"><input class="mr-1" type="radio" name="search" v-model="searchKey" @change="searchUser" value="1" checked>全て</label>
+            <label class="mx-2"><input class="mr-1" type="radio" name="search" v-model="searchKey" @change="searchUser" value="2">管理者</label>
+            <label class="mx-2"><input class="mr-1" type="radio" name="search" v-model="searchKey" @change="searchUser" value="3">スタッフ</label>
+            <label class="mx-2">ソート</label>
+            <label class="mx-2"><input class="mr-1" type="radio" name="sort" v-model="sortKey" @change="sortUser" value="1" checked>スタッフ名</label>
+            <label class="mx-2"><input class="mr-1" type="radio" name="sort" v-model="sortKey" @change="sortUser" value="2">入社日</label>
             <input type="button" class="btn btn-primary" value="新規追加" @click="openRegisterModal()">
           </div>
         </div>
@@ -27,7 +34,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="staff in staffs" v-bind:key="staff.uid">
+            <tr v-for="staff in dispStaffs" v-bind:key="staff.uid">
               <td class="width-12 text-center">{{ staff.staffName }}</td>
               <td class="width-20 text-center">{{ staff.email }}</td>
               <td class="width-12 text-center">{{ setDate(staff.joiningDate) }}</td>
@@ -62,11 +69,24 @@ export default {
   },
   created() {
     this.staffs = this.$store.getters.getStaffs;
+    this.searchUser();
   },
   data () {
     return {
       staffs: [],
+      dispStaffs: [],
       targetStaff: null,
+      searchKey: '1',
+      useSearchKey: {
+        all: '1',
+        admin: '2',
+        staff: '3'
+      },
+      sortKey: '1',
+      userSortKey: {
+        staffName: '1',
+        joiningDate: '2'
+      },
       errorMessage: '',
       isDispRegister: false,
       isDispDelete: false
@@ -94,11 +114,43 @@ export default {
       if (staff.admin) return '－';
       else return `${staff.daysLeft}日`;
     },
+    searchUser() {
+      switch (this.searchKey) {
+        case this.useSearchKey.all:
+          this.dispStaffs = this.staffs;
+          break;
+        case this.useSearchKey.admin:
+          this.dispStaffs = this.staffs.filter(x => x.admin);
+          break;
+        case this.useSearchKey.staff:
+          this.dispStaffs = this.staffs.filter(x => !x.admin);
+          break;
+      }
+      this.sortUser();
+    },
+    // ソート機能
+    sortUser() {
+      switch (this.sortKey) {
+        case this.userSortKey.staffName:
+          this.dispStaffs.sort( (a, b) => {
+            if(a.staffName > b.staffName) return 1;
+            if(a.staffName < b.staffName) return -1;
+          });
+          break;
+        case this.userSortKey.joiningDate:
+          this.dispStaffs.sort( (a, b) => {
+            if(a.joiningDate > b.joiningDate) return 1;
+            if(a.joiningDate < b.joiningDate) return -1;
+          });
+          break;
+      }
+    },
     // アカウントを作成
     async registerAccount(item) {
       try {
         await this.$store.dispatch('registerAccount', item);
         this.staffs = this.$store.getters.getStaffs;
+        this.searchUser();
         this.errorMessage = '';
       } catch (error) {
         this.errorMessage = '登録に失敗しました';
@@ -111,6 +163,7 @@ export default {
       try {
         await this.$store.dispatch('deleteAccount', (this.targetStaff.uid));
         this.staffs = this.$store.getters.getStaffs;
+        this.searchUser();
         this.errorMessage = '';
       } catch (error) {
         this.errorMessage = '削除に失敗しました';
